a9a2bdf9e99569810a80eb9f0162149973666351
ravionthebeat <illravimc@gmail.com>
2021-08-06 19:29:02 -0300

    HOMEWORK1.js

---
+++ 02-JS-I/homework/HOMEWORK1.js
@@ -0,0 +1,57 @@
+HOMEWORK 1
+
+Variable:
+Una Variable es una forma de guardar algo para usarlo mas tarde.
+Una variable puede ser ajustada o reestablecida sin dar a conocer de que tipo es
+las Variables se abrevian en las siglas "var" seguida de un espacio y el nombre de la misma.
+
+String:
+
+Las "String" son bloques de texto, siempre van entre comillas, sean simples o dobles ('' o "")
+
+cualquier texto o numero que esté entre comillas son "String".
+
+Funciones:
+
+Las funciones son unas de las partes mas importantes del lenguaje de programación.
+Las funciones son objetos "particulares" y obtienen las mismas características de los objetos comunes.
+
+Argumentos:
+
+Si le damos a una variable entre paréntesis una razón u objeto dentro de una función, podremos usar esta variable
+dentro de nuestra función, a eso se le llama "Argumento".
+
+Return:
+
+El return se utiliza para que la función que estamos ejecutando, se frene y nos de una "devolución"
+
+
+Comparación:
+
+Los operadores de comparación permiten evaluar dos elementos y devuelven si el resultado de este es verdadero o falso. Estos operadores son: <, <=, >, > =, ===, ! ==.
+
+
+El flujo de control es una forma de que nuestra función compruebe si algo es verdadero o falso.
+
+IF:
+Un IF en programación se utiliza para evaluar una condición:
+
+Si es correcta la condición dicha: correrá un bloque de codigo
+
+de ser falsa el programa omitirá el codigo y devolverá 'false'.
+
+Valores Booleanos:
+
+Los valores Booleanos vienen de la lógica de Boole y estos se guían a traves de números binarios (0 o 1)
+
+Normalmente en Javascript usamos booleanos para significar verdadero o falso.
+
+-----------FINISH----------
+
+
+
+
+
+
+
+
