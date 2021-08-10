293e3593d7379df407b8ecfdc638c897cd45e76a
ravionthebeat <illravimc@gmail.com>
2021-08-10 16:17:39 -0300

    homework1.js

---
+++ 05-JS-IV/homework/homework1.js
@@ -0,0 +1,24 @@
+Homework1
+
+Objetos:
+Un Objeto es otro contenedor de datos (como los arrays y las matrices.)
+Un objeto contiene mucha información sobre una sola cosa.
+Los objetos se invocan con {} (corchetes)
+
+Propiedades:
+Una propiedad es un identificador con un determinado tipo de dato que accede normalmente a un campo en forma directa o a través de un método. (nombres, sustantivos, cosas, etc.)
+
+Métodos:
+ En los objetos, los valores se pueden poner en funciones, las funciones guardadas en objetos les decimos "métodos".
+vimos muchos métodos como (.length, .push, .pop, .etc).
+
+Bucle 'For ...in':
+
+el bucle 'for-in' itera (Que llega a un resultado mediante aproximaciones sucesivas) sobre todas las propiedades enumerables de un objeto que está codificado por cadenas
+
+Notacion de puntos vs Notación de Corchetes:
+
+Con la notación de puntos podemos llamar al nombre del objeto, un punto y el nombre de la clave.
+Así como llamamos a la propiedad .length en una matriz.
+En cambio, con a notación de corchetes es como llamar a un elemento en una matriz, aunque con corchetes debemos usar una cadena o número, o una variable que apunte a una cadena o número.
+Se puede llamar a cada clave envolviéndola con comilla
