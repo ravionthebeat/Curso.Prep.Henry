b53133ed25660e5977c44ab9c688f650a2a143be
ravionthebeat <illravimc@gmail.com>
2021-08-10 15:51:53 -0300

    Homework1.js

---
+++ 06-JS-V/homework/Homework1.js
@@ -0,0 +1,11 @@
+Homework1
+
+1: Prototypes (__proto__)
+
+los prototipos o "proto" sirven para evitar la acumulación de métodos, por lo tanto la memoria de nuestro equipo no tendrá que ser ocupada en exceso.
+}
+los prototipos se usan para "acortar" los metodos que los objetos de class necesitan.
+
+2: __Constructors__ (de clases)
+
+Muchas veces cuando creamos un objeto, estamos creando una plantilla. En lugar de copiar esa plantilla una y otra vez, Javascript nos da acceso a lo que llamamos un constructor o class. Las clases comparten gran parte de la misma funcionalidad que los objetos normales, pero también van mucho mas allá en esa funcionalidad. Las clases son útiles para crear muchos objetos que comparten algunas de las mismas "propiedades y métodos" como un usuario en un sitio web.
