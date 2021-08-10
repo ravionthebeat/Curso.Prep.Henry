3fc4e2d63a8e607b746ee1aa40cbe866aaa0e8c4
ravionthebeat <illravimc@gmail.com>
2021-08-10 17:07:45 -0300

    Homework1.js

---
+++ 03-JS-II/homework/Homework1.js
@@ -0,0 +1,16 @@
+Homework1
+
+'Bucles For' 
+
+los bucles "FOR" 
+Los bucles for tienen una sintaxis única, similar a la instrucción if, pero un poco más compleja.
+Primero tenemos la palabra clave for, seguida de paréntesis y luego abrir y cerrar llaves.
+Dentro de los paréntesis necesitaremos tres cosas.
+Primero, debemos declarar una variable, esto es sobre lo que se repetirá el bucle.
+Entonces tendremos una expresión condicional, el ciclo continuará sucediendo hasta que esta declaración sea false.
+Tercero, incrementaremos nuestra variable. Las tres declaraciones están separadas por un punto y coma. Por ejemplo:
+
+for (let i = 0                 ; i < 10                 ; i++          ) {
+// | Declaramos una variable | Expresión condcicional | Incrementamos la variable |
+    console.log(i);
+}
