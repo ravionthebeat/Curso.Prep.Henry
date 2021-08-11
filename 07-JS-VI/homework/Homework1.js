03483436109cdeb4c23a02776677160e24f46e42
ravionthebeat <illravimc@gmail.com>
2021-08-11 11:16:12 -0300

    Homework1.js

---
+++ 07-JS-VI/homework/Homework1.js
@@ -0,0 +1,6 @@
+Homework1
+
+Funciones callback:
+
+Un concepto muy importante en Javascript es la capacidad de pasar una función como argumento a otra función. Estas funciones se denominan callbacks. Estas funciones pueden llamarse en cualquier momento y pasar argumentos dentro de la función.
+La convención es usar cb como argumento para la variable que se usará de callback.
